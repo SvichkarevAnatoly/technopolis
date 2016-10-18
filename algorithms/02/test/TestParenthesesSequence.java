770a0c515c239496ee16f5c10cf4372e756df089
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-18 20:30:55 +0300

    протестировал ParenthesesSequence

---
+++ algorithms/02/test/TestParenthesesSequence.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static other.ParenthesesSequence.isBalanced;
+
+public class TestParenthesesSequence {
+    @Test
+    public void isBalanced1() {
+        final String sequence = "()()";
+        assertTrue(isBalanced(sequence));
+    }
+
+    @Test
+    public void isBalanced2() {
+        final String sequence = "((((";
+        assertFalse(isBalanced(sequence));
+    }
+
+    @Test
+    public void isBalanced3() {
+        final String sequence = ")()(";
+        assertFalse(isBalanced(sequence));
+    }
+}
