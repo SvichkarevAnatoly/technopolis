4e0ac185310f32da37f9126561f13e361a0379da
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-21 19:06:59 +0300

    решение задачи Сумма двух

--- algorithms/labs-1/src/SumAB.java
+++ algorithms/labs-1/src/SumAB.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
+// https://www.e-olymp.com/ru/problems/518
 public class SumAB {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        final int A = scanner.nextInt();
-        final int B = scanner.nextInt();
-        System.out.println(A + B);
+        final int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            final int A = scanner.nextInt();
+            final int B = scanner.nextInt();
+            System.out.println(A + B);
+        }
     }
 }
