87f80d1bb7678610c8e338496a4cbb6ef7751820
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-21 18:46:16 +0300

    решение задачи A+B

---
+++ algorithms/labs-1/src/SumAB.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SumAB {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int A = scanner.nextInt();
+        final int B = scanner.nextInt();
+        System.out.println(A + B);
+    }
+}
