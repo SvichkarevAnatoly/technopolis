bf545f364e8eb5825262046c3ea446f617569448
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-21 10:36:20 +0300

    исправил игнорирование всех out директорий с бинарниками

---
+++ algorithms/labs-1/src/CheckSimplicity.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CheckSimplicity{
+    public static boolean check(int n) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+
+    }
+}
---
+++ algorithms/labs-1/test/CheckSimplicityTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class CheckSimplicityTest {
+    @Test
+    public void nEquals2() {
+        final int n = 2;
+
+    }
+}
