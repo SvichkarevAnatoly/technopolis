69f7a47b390da86136151b11c3f65e732b689fac
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-24 17:30:01 +0300

    сдал на 100% задачу Верёвочки

--- algorithms/labs-1/src/Ropes.java
+++ algorithms/labs-1/src/Ropes.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 // https://www.e-olymp.com/ru/problems/5333
 public class Ropes {
-    public static int getMaxLength(int[] ropeLengths, int k) {
-        final int maxBound = getMaxBound(ropeLengths, k);
+    public static long getMaxLength(long[] ropeLengths, int k) {
+        final long maxBound = getMaxBound(ropeLengths, k);
         if (maxBound == 0) {
             return 0;
         }
         final int checkMaxBound = checkLength(maxBound, ropeLengths, k);
-        if (checkMaxBound == 0) {
+        if (checkMaxBound >= 0) {
             return maxBound;
         }
 
         return binarySearch(ropeLengths, k, maxBound);
     }
 
-    public static int binarySearch(int[] ropeLengths, int k, int maxBound) {
-        int l = 1, r = maxBound;
+    public static long binarySearch(long[] ropeLengths, int k, long maxBound) {
+        long l = 1, r = maxBound;
         while (l < r - 1) {
-            int checkedLength = (l + r) / 2;
+            long checkedLength = (l + r) / 2;
             switch (checkLength(checkedLength, ropeLengths, k)) {
                 case -1:
                     r = checkedLength;
@@ -32,17 +32,17 @@ public class Ropes {
         return l;
     }
 
-    public static int getMaxBound(int[] ropeLengths, int k) {
-        int maxBound = 0;
-        for (int ropeLength : ropeLengths) {
+    public static long getMaxBound(long[] ropeLengths, int k) {
+        long maxBound = 0;
+        for (long ropeLength : ropeLengths) {
             maxBound += ropeLength;
         }
         return maxBound / k;
     }
 
-    public static int checkLength(int checkedLength, int[] ropeLengths, int k) {
+    public static int checkLength(long checkedLength, long[] ropeLengths, int k) {
         int partNumber = 0;
-        for (int ropeLength : ropeLengths) {
+        for (long ropeLength : ropeLengths) {
             partNumber += ropeLength / checkedLength;
         }
         return Integer.compare(partNumber, k);
@@ -52,11 +52,11 @@ public class Ropes {
         final Scanner scanner = new Scanner(System.in);
         final int n = scanner.nextInt();
         final int k = scanner.nextInt();
-        final int[] ropeLengths = new int[n];
+        final long[] ropeLengths = new long[n];
         for (int i = 0; i < n; i++) {
-            ropeLengths[i] = scanner.nextInt();
+            ropeLengths[i] = (int) scanner.nextLong();
         }
-        final int maxLength = getMaxLength(ropeLengths, k);
+        final long maxLength = getMaxLength(ropeLengths, k);
         System.out.println(maxLength);
     }
 }
