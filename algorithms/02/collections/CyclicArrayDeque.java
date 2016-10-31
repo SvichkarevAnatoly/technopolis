49805b12e4f58b79fcb812b593129ab314335809
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-31 18:59:06 +0300

    решение задачи CyclicArrayDeque и тесты

--- algorithms/02/collections/CyclicArrayDeque.java
+++ algorithms/02/collections/CyclicArrayDeque.java
@@ -3,62 +3,117 @@ package collections;
 import java.util.Iterator;
 
 public class CyclicArrayDeque<Item> implements IDeque<Item> {
+    private static final int DEFAULT_CAPACITY = 10;
 
-    private Item[] elementData;
+    @SuppressWarnings("unchecked")
+    private Item[] elementData = (Item[]) new Object[DEFAULT_CAPACITY];
+    private int head, tail;
 
     @Override
     public void pushFront(Item item) {
-        /* TODO: implement it */
+        grow();
+        if (head == 0) {
+            head = elementData.length - 1;
+        } else {
+            head--;
+        }
+        elementData[head] = item;
     }
 
     @Override
     public void pushBack(Item item) {
-        /* TODO: implement it */
+        grow();
+        elementData[tail] = item;
+        tail = (tail + 1) % elementData.length;
     }
 
     @Override
     public Item popFront() {
-        /* TODO: implement it */
-        return null;
+        shrink();
+        final Item item = elementData[head];
+        head = (head + 1) % elementData.length;
+        return item;
     }
 
     @Override
     public Item popBack() {
-        /* TODO: implement it */
-        return null;
+        shrink();
+        if (tail == 0) {
+            tail = elementData.length - 1;
+        } else {
+            tail--;
+        }
+        return elementData[tail];
     }
 
     @Override
     public boolean isEmpty() {
-        /* TODO: implement it */
-        return false;
+        return size() == 0;
     }
 
     @Override
     public int size() {
-        /* TODO: implement it */
-        return 0;
+        if (tail >= head) {
+            return tail - head;
+        } else {
+            return (elementData.length - head) + tail;
+        }
     }
 
     private void grow() {
-        /**
-         * TODO: implement it
-         * Если массив заполнился,
-         * то увеличить его размер в полтора раз
-         */
+        if (size() == elementData.length - 1) {
+            int oldCapacity = elementData.length;
+            // <=> * 1.5
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            changeCapacity(newCapacity);
+        }
     }
 
     private void shrink() {
-        /**
-         * TODO: implement it
-         * Если количество элементов в четыре раза меньше,
-         * то уменьшить его размер в два раза
-         */
+        int oldCapacity = elementData.length;
+        // не уменьшать размер меньше, чем размер по умолчанию
+        if (elementData.length >= DEFAULT_CAPACITY &&
+                size() == oldCapacity >> 2) {
+            changeCapacity(oldCapacity >> 1);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void changeCapacity(int newCapacity) {
+        final Item[] newElementData = (Item[]) new Object[newCapacity];
+        if (tail >= head) {
+            System.arraycopy(elementData, head, newElementData, 0, size());
+            tail -= head;
+            head = 0;
+        } else {
+            System.arraycopy(elementData, 0, newElementData, 0, tail);
+            final int delta = newCapacity - elementData.length;
+            System.arraycopy(elementData, head, newElementData, delta + head, elementData.length - head);
+            head += delta;
+        }
+        elementData = newElementData;
     }
 
     @Override
     public Iterator<Item> iterator() {
-        /* TODO: implement it */
-        return null;
+        return new CyclicArrayDequeIterator();
+    }
+
+    private class CyclicArrayDequeIterator implements Iterator<Item> {
+
+        private int currentPosition = head;
+
+        @Override
+        public boolean hasNext() {
+            return currentPosition != tail;
+        }
+
+        @Override
+        public Item next() {
+            final Item item = elementData[currentPosition];
+            currentPosition = (currentPosition + 1) % elementData.length;
+            return item;
+        }
+
     }
 }
---
+++ algorithms/02/test/collections/CyclicArrayDequeTest.java
@@ -0,0 +1,135 @@
+package test.collections;
+
+import collections.CyclicArrayDeque;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class CyclicArrayDequeTest {
+    private CyclicArrayDeque<Integer> deque;
+
+    @Before
+    public void setUp() throws Exception {
+        deque = new CyclicArrayDeque<>();
+    }
+
+    @Test
+    public void constructor() {
+        assertTrue(deque.isEmpty());
+        assertEquals(0, deque.size());
+    }
+
+    @Test
+    public void pushBackPopFront() {
+        deque.pushBack(5);
+        assertEquals(5, deque.popFront().intValue());
+    }
+
+    @Test
+    public void pushFrontPopBack() {
+        deque.pushFront(5);
+        assertEquals(5, deque.popBack().intValue());
+    }
+
+    @Test
+    public void pushBackPopFrontDouble() {
+        deque.pushBack(5);
+        assertEquals(5, deque.popFront().intValue());
+        deque.pushBack(7);
+        assertEquals(7, deque.popFront().intValue());
+    }
+
+    @Test
+    public void allPushPop() {
+        deque.pushFront(2);
+        deque.pushFront(1);
+        deque.pushBack(3);
+        assertEquals(1, deque.popFront().intValue());
+        assertEquals(3, deque.popBack().intValue());
+        assertEquals(2, deque.popFront().intValue());
+    }
+
+    @Test
+    public void pushBackManyItems() {
+        for (int i = 0; i < 20; i++) {
+            deque.pushBack(i);
+        }
+        assertEquals(20, deque.size());
+        for (int i = 0; i < 20; i++) {
+            assertEquals(i, deque.popFront().intValue());
+        }
+    }
+
+    @Test
+    public void pushBackHeadGreaterThanTail() {
+        for (int i = 0; i < 7; i++) {
+            deque.pushBack(i);
+            deque.popFront();
+        }
+        assertTrue(deque.isEmpty());
+        for (int i = 0; i < 20; i++) {
+            deque.pushBack(i);
+        }
+        for (int i = 0; i < 20; i++) {
+            assertEquals(i, deque.popFront().intValue());
+        }
+    }
+
+    @Test
+    public void size2() {
+        deque.pushBack(5);
+        deque.pushBack(7);
+        assertEquals(2, deque.size());
+        assertEquals(5, deque.popFront().intValue());
+        assertEquals(7, deque.popFront().intValue());
+    }
+
+    @Test
+    public void sizeIsEmpty() {
+        deque.pushBack(5);
+
+        assertEquals(1, deque.size());
+        assertFalse(deque.isEmpty());
+
+        deque.popFront();
+        assertEquals(0, deque.size());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void iterator() {
+        deque.pushBack(5);
+        deque.pushBack(7);
+
+        final Iterator<Integer> iterator = deque.iterator();
+        assertTrue(iterator.hasNext());
+        assertEquals(5, iterator.next().intValue());
+        assertEquals(7, iterator.next().intValue());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void iteratorHeadGreaterThanTail() {
+        final CyclicArrayDeque<Integer> Deque = new CyclicArrayDeque<>();
+        for (int i = 0; i < 7; i++) {
+            deque.pushFront(i);
+            deque.popBack();
+        }
+        assertTrue(Deque.isEmpty());
+        for (int i = 0; i < 20; i++) {
+            Deque.pushBack(i);
+        }
+
+        final Iterator<Integer> iterator = Deque.iterator();
+        assertTrue(iterator.hasNext());
+        int i = 0;
+        while (iterator.hasNext()) {
+            assertEquals(i, iterator.next().intValue());
+            i++;
+        }
+        assertFalse(iterator.hasNext());
+    }
+}
