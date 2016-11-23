999ee047fb6b97782e2e6bf8c150d886160dcb3d
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-23 12:02:08 +0300

    неработающая Сортировка подсчетом

---
+++ algorithms/labs-2/src/CountingSort.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CountingSort {
+    public static final int RANGE = 100000;
+
+    public static int[] sort(int[] a, int k) {
+        int c[] = new int[k];
+        for (int i = 0; i < a.length; i++)
+            c[a[i]]++;
+        for (int i = 1; i < k; i++)
+            c[i] += c[i-1];
+        int b[] = new int[a.length];
+        for (int i = a.length-1; i >= 0; i--)
+            b[--c[a[i]]] = a[i];
+        return b;
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        boolean hasNext() {
+            return st.hasMoreTokens();
+        }
+    }
+
+    public static void main(String[] args) {
+        final FastScanner scanner = new FastScanner();
+        final int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        arr = sort(arr, RANGE);
+        final PrintWriter printWriter = new PrintWriter(System.out);
+        for (int i = 0; i < n; i++) {
+            printWriter.print(arr[i]);
+            printWriter.print(' ');
+        }
+        printWriter.close();
+    }
+}
