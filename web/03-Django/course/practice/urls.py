84e6b1012cc8b55bea319d53ca3211d995d71b18
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-30 20:06:15 +0300

    изменение студента

--- web/03-Django/course/practice/urls.py
+++ web/03-Django/course/practice/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'practice'
 urlpatterns = [
-    url(r'^students/$', views.students, name='students'),
+    url(r'^student/$', views.get_student_list, name='student'),
+    url(r'^student/edit/(?P<id>[0-9]+)/$', views.edit_student, name='edit_student'),
+    url(r'^student/save/(?P<id>[0-9]+)/$', views.save_student, name='save_student'),
 ]
--- web/03-Django/course/practice/views.py
+++ web/03-Django/course/practice/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Student
 
 
-def students(request):
+def get_student_list(request):
     student_list = Student.objects.all()
     context = {'student_list': student_list}
 
-    return render(request, "practice/index.html", context)
+    return render(request, "practice/students.html", context)
+
+
+def edit_student(request, id):
+    student = get_object_or_404(Student, pk=id)
+    context = {'student': student}
+    return render(request, 'practice/edit_student.html', context)
+
+
+def save_student(request, id):
+    student = get_object_or_404(Student, pk=id)
+    try:
+        student.name = request.POST['name']
+        student.family = request.POST['family']
+        student.save()
+    except KeyError:
+        context = {'student': student}
+        return render(request, 'practice/edit_student.html', context)
+    else:
+        return HttpResponseRedirect(reverse('practice:student'))
