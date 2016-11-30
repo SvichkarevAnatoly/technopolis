6d620c67f3bbda498187f3d80a148e3a9eec88da
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-30 16:31:28 +0300

    рефакторинг

--- web/03-Django/course/practice/views.py
+++ web/03-Django/course/practice/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Student
 
 
 def students(request):
     student_list = Student.objects.all()
-
-    template = loader.get_template("practice/index.html")
     context = {'student_list': student_list}
-    return HttpResponse(template.render(context, request))
+
+    return render(request, "practice/index.html", context)
