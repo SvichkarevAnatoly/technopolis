fe220ada788e551e43be62981e19e7281d2fa782
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-30 16:27:53 +0300

    отображение через шаблон списка студентов

--- web/03-Django/course/practice/views.py
+++ web/03-Django/course/practice/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Student
 
 
 def students(request):
     student_list = Student.objects.all()
-    output = "<br>".join(map(str, student_list))
-    return HttpResponse(output)
+
+    template = loader.get_template("practice/index.html")
+    context = {'student_list': student_list}
+    return HttpResponse(template.render(context, request))
